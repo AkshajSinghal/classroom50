160f304ded9f0f5633e01010b808b78923b37a64
Colton Ogden <3724177+coltonoscopy@users.noreply.github.com>
2026-05-24 22:26:28 -0700

    csv parsing and student # in classes

---
+++ src/hooks/useGetStudents.ts
@@ -0,0 +1,30 @@
+import { useQuery } from "@tanstack/react-query"
+
+import { useGitHubClient } from "@/context/github/GitHubProvider"
+import { csvFileQuery } from "./github/queries"
+
+type Student = {
+  username: string
+  first_name: string
+  last_name: string
+  email: string
+  section: string
+  github_id: string
+}
+const useGetStudents = (org, classroom) => {
+  const client = useGitHubClient()
+  const { data: students } = useQuery(
+    csvFileQuery<Student>(
+      client,
+      org,
+      "classroom50",
+      `${classroom}/students.csv`,
+    ),
+  )
+
+  return {
+    students: students || [],
+  }
+}
+
+export default useGetStudents
