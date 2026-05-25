73fbde68e13e4a4825737a9a5ae0f440190cf7bb
Colton Ogden <3724177+coltonoscopy@users.noreply.github.com>
2026-05-24 18:40:10 -0700

    assignments fetching and linking to assignments page

--- src/pages/ClassesPage.tsx
+++ src/pages/ClassesPage.tsx
@@ -1,8 +1,9 @@
+import { useEffect } from "react"
 import { useParams, Link } from "@tanstack/react-router"
 import { BookText, UsersRound } from "lucide-react"
 import GitHub from "@/assets/github.svg?react"
 
-import { useCourseTeacherAccess } from "@/hooks/useCourseTeacherAccess"
+import useGetClassroomAssignments from "@/hooks/useGetClassAssignments"
 import useGetClasses from "@/hooks/useGetClasses"
 
 import Drawer, {
@@ -43,6 +44,16 @@ const classes = [
 ]
 
 const ClassCard = ({ cl, org }: { cl: any; org: string }) => {
+  const { data: classData } = useGetClassroomAssignments(org, cl.path)
+
+  useEffect(() => {
+    console.log("class", cl)
+  }, [cl])
+
+  useEffect(() => {
+    console.log("classData", classData)
+  }, [classData])
+
   return (
     <div className="card bg-base-100 rounded-xl col-span-6 border border-[#eee]">
       <div className="card-body gap-4">
@@ -62,10 +73,10 @@ const ClassCard = ({ cl, org }: { cl: any; org: string }) => {
           <GitHub className="size-4 opacity-25" />
           <pre>{cl.org || "No Org Specified"}</pre>
         </div>
-        {cl.short_name ? (
+        {classData?.assignments.length ? (
           <Link
             type="button"
-            to={`/${org}/${cl.short_name}`}
+            to={`/${org}/${cl.path}/assignments`}
             className="btn btn-outline btn-primary w-full"
           >
             <BookText />
@@ -103,7 +114,7 @@ const ClassesPage = () => {
           </div>
           <div className="grid grid-cols-12 gap-4 mb-6">
             {classesData
-              ?.filter((cl) => cl.type !== "dir" && cl.name !== ".github")
+              ?.filter((cl) => cl.type === "dir" && cl.name !== ".github")
               .map((cl) => (
                 <ClassCard cl={cl} org={params.org} />
               ))}
