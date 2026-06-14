9698c167942b28138a479df27e043a77cf85096a
Colton Ogden <coltonoscopy@gmail.com>
2026-06-13 20:31:53 -0700

    better name handling for avatars and names in roster view

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -1,16 +1,24 @@
 import GitHub from "@/assets/github.svg?react"
 
-const Avatar = ({ initials, name, github }) => {
+const Avatar = ({
+  initials,
+  name,
+  github,
+}: {
+  initials: string
+  name: string
+  github: string
+}) => {
   return (
     <div className="flex gap-3">
       <div className="avatar avatar-placeholder">
         <div className="bg-base-200 text-primary rounded-full w-12">
-          <span>{initials}</span>
+          <span>{initials || github.at(0)?.toUpperCase()}</span>
         </div>
       </div>
 
       <div className="min-w-0 flex-1">
-        <div className="font-medium text-base-content">{name}</div>
+        <div className="font-medium text-base-content">{name || github}</div>
 
         <div className="flex items-center gap-1 text-sm text-base-content/60">
           <GitHub className="size-4" />
