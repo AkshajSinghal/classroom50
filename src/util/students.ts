b33852803b7a39e156d3ea8d12228f1def04618d
Colton Ogden <3724177+coltonoscopy@users.noreply.github.com>
2026-06-13 20:31:53 -0700

    better name handling for avatars and names in roster view

--- src/util/students.ts
+++ src/util/students.ts
@@ -1,12 +1,28 @@
+import type { Student } from "@/types/classroom"
+
 export const capitalize = (s: string) =>
   s ? s.charAt(0).toUpperCase() + s.slice(1) : ""
 
-export const getName = (key: string, students) => {
+export const getName = (key: string, students: Student[]) => {
   const student = students.find((s) => s.username === key)
-  return `${capitalize(student?.first_name)} ${capitalize(student?.last_name.slice(0, 1)) + "."}`
+  if (!student) return ""
+
+  const { first_name, last_name } = student
+
+  if (!first_name && !last_name) {
+    return ""
+  }
+
+  if (!first_name) return capitalize(last_name)
+  if (!last_name) return capitalize(first_name)
+
+  return `${capitalize(first_name)} ${capitalize(last_name.slice(0, 1)) + "."}`
 }
 
-export const getInitials = (key: string, students) => {
+export const getInitials = (key: string, students: Student[]) => {
   const student = students.find((s) => s.username === key)
-  return `${capitalize(student?.first_name.slice(0, 1)) + capitalize(student?.last_name.slice(0, 1))}`
+  if (!student) return ""
+  const { first_name, last_name } = student
+
+  return `${capitalize(first_name.slice(0, 1)) + capitalize(last_name.slice(0, 1))}`
 }
