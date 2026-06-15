1689a138710cec35343fcce84ab408d9de62cd67
Colton Ogden <coltonoscopy@gmail.com>
2026-06-15 08:48:44 -0700

    conditional rendering of teacher-specific things as student

--- src/pages/OrgPage.tsx
+++ src/pages/OrgPage.tsx
@@ -2,11 +2,19 @@ import { useParams } from "@tanstack/react-router"
 
 import { useCourseTeacherAccess } from "@/hooks/useCourseTeacherAccess"
 import useGetClasses from "@/hooks/useGetClasses"
+import { useEffect } from "react"
 
 const OrgPage = () => {
   const params = useParams({ from: "/$org/" })
   const { isTeacher, isStudent, isBlocked } = useCourseTeacherAccess(params.org)
   const { data: classesData } = useGetClasses(params.org)
+
+  useEffect(() => {
+    console.log("is teacher", isTeacher)
+    console.log("is student", isStudent)
+    console.log("is blocked", isBlocked)
+    console.log("classes data", classesData)
+  }, [isTeacher, isStudent, isBlocked, classesData])
 
   return (
     <div>
